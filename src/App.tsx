7e8a800bc93ed05fcd8eb98d037185d526439412
Stepan Startsev <startsev.sv@perfema.com>
2021-10-04 20:38:51 +0200

    init

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React, { FC } from "react";
-import { Main } from "views/screens/Main";
-
-export const App: FC = () => {
-    return <Main />;
-};
--- src/utils/c.ts
+++
@@ -1,8 +0,0 @@
-export type ClassMap = { [key: string]: boolean | undefined | null | false };
-
-export const c = (...classes: (ClassMap | string | undefined | null | false)[]): string =>
-    (classes
-        .filter(x => x !== null && x !== undefined && x !== false) as (ClassMap | string)[])
-        .flatMap(x => typeof(x) === "string"
-            ? [x] : Object.entries(x).filter(([, r]) => r).map(([cl]) => cl))
-        .join(" ");
